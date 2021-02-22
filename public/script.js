8a840515e8e2faa830cd0489fb12f54faf996089
michelle jae <moyer.mj@gmail.com>
2021-02-22 00:23:08 -0500

    close button

--- public/script.js
+++ public/script.js
@@ -1,6 +1,15 @@
 let ngnixButton = document.querySelector('#ngnix');
 let ngnixModal = document.querySelector('#nginx-image');
+let textContainer = document.querySelector('#text-container');
+let closeButton = document.querySelector('.close');
+let modal = document.querySelector(".modal")
 
 ngnixButton.addEventListener('click', () => {
     ngnixModal.classList.toggle("off");
+    textContainer.classList.toggle("blur");
+})
+
+closeButton.addEventListener('click', () => {
+    modal.classList.toggle("off");
+    textContainer.classList.toggle("blur");
 })
