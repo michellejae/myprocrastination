6c9270bfbfb26bc4ddd18c47563916e455219415
michelle jae <moyer.mj@gmail.com>
2021-02-21 23:55:33 -0500

    ngnix modal

---
+++ public/script.js
@@ -0,0 +1,6 @@
+let ngnixButton = document.querySelector('#ngnix');
+let ngnixModal = document.querySelector('#nginx-image');
+
+ngnixButton.addEventListener('click', () => {
+    ngnixModal.classList.toggle("off");
+})
