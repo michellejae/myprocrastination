ecb58d60e43f692f5227a7c5c813f9d0b8833c1a
michelle jae <moyer.mj@gmail.com>
2021-02-22 00:44:02 -0500

    restructure and typo lol

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 ##### Start
+
+## TODO
+- need to change images in modals to img tags instead of backgroundi imgs
+- try to get modals to pop up from their specific 'circles'
+- mobil version
+- go back to my profile link
+
+## ISSUES
+- i cannot close out of popup photos with X button. it only works for the first one. 
+- me photo is massive cause i'm tired and it's 1am and my brain willl not work
--- public/script.js
+++ public/script.js
@@ -1,11 +1,27 @@
 let ngnixButton = document.querySelector('#ngnix');
 let ngnixModal = document.querySelector('#nginx-image');
+let securityButton = document.querySelector('#security');
+let securityModal = document.querySelector('#security-image');
+let meButton = document.querySelector('#me');
+let meModal = document.querySelector('#me-image');
+
 let textContainer = document.querySelector('#text-container');
 let closeButton = document.querySelector('.close');
-let modal = document.querySelector(".modal")
+let modal = document.querySelector(".modal");
+
 
 ngnixButton.addEventListener('click', () => {
     ngnixModal.classList.toggle("off");
+    textContainer.classList.toggle("blur");
+})
+
+securityButton.addEventListener('click', () => {
+    securityModal.classList.toggle("off");
+    textContainer.classList.toggle("blur");
+})
+
+meButton.addEventListener('click', () => {
+    meModal.classList.toggle("off");
     textContainer.classList.toggle("blur");
 })
 
